77232c00b5c4231215ca4318890089c64642e238
gaIIego <141265622+gaIIego@users.noreply.github.com>
2023-08-03 21:22:00 -0300

    Add files via upload

---
+++ servicios.js
@@ -0,0 +1,9 @@
+function mostrarMenu() {
+    if (menuImagenes.style.display === "none") {
+        // Si el menú está oculto, lo mostramos
+        menuImagenes.style.display = "block";
+    } else {
+        // Si el menú está visible, lo ocultamos
+        menuImagenes.style.display = "none";
+    }
+}
